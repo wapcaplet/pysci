d1cdb7cfb8f43b923b73d2d7c665ca0d54a5cd1b
Eric Pierce <wapcaplet88@gmail.com>
2011-07-05 21:05:57 -0600

    Added quit menu entry with keyboard shortcut

--- demo.py
+++ demo.py
@@ -7,7 +7,7 @@
 import sys
 from pysci.editor import PySci
 from pysci.config import EditorSettings
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
 
 class Demo (QtGui.QMainWindow):
     def __init__(self):
@@ -16,12 +16,26 @@ class Demo (QtGui.QMainWindow):
 
         self.editor = PySci(self)
 
+        self._create_menubar()
+
+
+    def _create_menubar(self):
+        """Create the application menu bar.
+        """
         menubar = self.menuBar()
+        quit = QtGui.QAction(QtGui.QIcon('icons/exit.png'), 'Quit', self)
+        quit.setShortcut('Ctrl+Q')
+        self.connect(quit, QtCore.SIGNAL('triggered()'), QtCore.SLOT('close()'))
+
+        menubar.addAction(quit)
         menubar.addAction('&Configure', self.configure)
 
         self.setCentralWidget(self.editor)
 
+
     def configure(self):
+        """Display the editor configuration window.
+        """
         settings = EditorSettings(self.editor)
         settings.exec_()
 
