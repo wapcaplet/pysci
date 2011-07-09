94b2541a94f21a719aaf4842ed08f94fd738bbb6
Eric Pierce <wapcaplet88@gmail.com>
2011-07-08 21:43:45 -0600

    Moved test to tests, added setup.py and .cfg

---
+++ setup.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+# setup.py
+
+"""Setup and installer for PySci.
+"""
+
+from distutils.core import setup
+
+setup(name='PySci',
+      version='0.1',
+      description='Pythonic interface to the QsciScintilla editor widget',
+      author='Eric Pierce',
+      author_email='wapcaplet88@gmail.com',
+      url='http://github.com/wapcaplet/pysci',
+      license='MIT License',
+      packages=['pysci'],
+     )
+
